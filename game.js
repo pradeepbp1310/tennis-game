06d3e2a38865446ae05203190244ab34a3ca0609
Pradeep B P <pradeepbp1310@gmail.com>
2019-08-15 21:18:17 +0530

    Implemented Moving ball x-axis and added player 1 paddle

--- game.js
+++ game.js
@@ -1,15 +1,30 @@
 let canvas, canvasContext;
+const PADDLE_HEIGHT = 100;
+const PADDLE_THICKNESS = 10;
+let ballX = 50;
+let ballXSpeed = 5;
+const INTERVAL_TIME = 1000;
 
 window.onload = function () {
+    let framePerSecond = 30;
     canvas = document.getElementById('gameCanvas');
     canvasContext = canvas.getContext('2d');
-    drawAllCanvas();
+
+    setInterval(() => {
+        moveCanvas();
+        drawAllCanvas()
+    }, INTERVAL_TIME / framePerSecond);
+}
+
+function moveCanvas() {
+    ballX += ballXSpeed;
+
 }
 
 function drawAllCanvas() {
-    createCanvas(0, 0, canvas.width, canvas.height);
+    createCanvas(0, 0, canvas.width, canvas.height, 'black');
     ballCanvas();
-
+    createCanvas(0, canvas.height / 2, PADDLE_THICKNESS, PADDLE_HEIGHT, 'white');
 }
 
 function createCanvas(x, y, width, height, color) {
@@ -20,6 +35,6 @@ function createCanvas(x, y, width, height, color) {
 function ballCanvas() {
     canvasContext.fillStyle = 'white';
     canvasContext.beginPath();
-    canvasContext.arc(100, 10, Math.PI * 2, 20, 10, true)
+    canvasContext.arc(ballX, 10, Math.PI * 2, 20, 10, true)
     canvasContext.fill();
 }
