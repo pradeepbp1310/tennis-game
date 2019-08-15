0516541c558af6ce255226a1233ed1dfef43953c
Pradeep B P <pradeepbp1310@gmail.com>
2019-08-15 21:24:28 +0530

    Bouncing ball horizontally and vertically

--- game.js
+++ game.js
@@ -1,9 +1,12 @@
 let canvas, canvasContext;
 const PADDLE_HEIGHT = 100;
 const PADDLE_THICKNESS = 10;
+const INTERVAL_TIME = 1000;
 let ballX = 50;
 let ballXSpeed = 5;
-const INTERVAL_TIME = 1000;
+let ballY = 50;
+let ballYSpeed = 5;
+
 
 window.onload = function () {
     let framePerSecond = 30;
@@ -18,6 +21,20 @@ window.onload = function () {
 
 function moveCanvas() {
     ballX += ballXSpeed;
+    ballY += ballYSpeed;
+    if (ballX < 0) {
+        ballXSpeed = -ballXSpeed;
+    }
+    if (ballX > canvas.width) {
+        ballXSpeed = - ballXSpeed
+    }
+    if (ballY < 0) {
+        ballYSpeed = -ballYSpeed;
+    }
+    if (ballY > canvas.height) {
+        ballYSpeed = - ballYSpeed
+    }
+
 
 }
 
@@ -35,6 +52,6 @@ function createCanvas(x, y, width, height, color) {
 function ballCanvas() {
     canvasContext.fillStyle = 'white';
     canvasContext.beginPath();
-    canvasContext.arc(ballX, 10, Math.PI * 2, 20, 10, true)
+    canvasContext.arc(ballX, ballY, Math.PI * 2, 20, 10, true)
     canvasContext.fill();
 }
