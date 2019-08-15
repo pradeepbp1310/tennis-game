ac27dd57405400304a56405c952c7afd7f21fa29
Pradeep B P <pradeepbp1310@gmail.com>
2019-08-15 21:44:28 +0530

    Implemented functionality - player1 paddle move using mousemove

--- game.js
+++ game.js
@@ -6,19 +6,27 @@ let ballX = 50;
 let ballXSpeed = 5;
 let ballY = 50;
 let ballYSpeed = 5;
-
+let paddleY1 = 0;
 
 window.onload = function () {
     let framePerSecond = 30;
     canvas = document.getElementById('gameCanvas');
     canvasContext = canvas.getContext('2d');
 
+    // Call move ball canvas and create other canvass
     setInterval(() => {
         moveCanvas();
         drawAllCanvas()
     }, INTERVAL_TIME / framePerSecond);
+
+    // Mouse move event listener
+    document.addEventListener('mousemove', (evt) => {
+        let mousePosition = calculateMousePosition(evt);
+        paddleY1 = mousePosition.y - (PADDLE_HEIGHT / 2);
+    })
 }
 
+// Canvas in action - move everything
 function moveCanvas() {
     ballX += ballXSpeed;
     ballY += ballYSpeed;
@@ -34,14 +42,15 @@ function moveCanvas() {
     if (ballY > canvas.height) {
         ballYSpeed = - ballYSpeed
     }
-
-
 }
 
 function drawAllCanvas() {
+    // Black background canvas
     createCanvas(0, 0, canvas.width, canvas.height, 'black');
+    // Ball canvas
     ballCanvas();
-    createCanvas(0, canvas.height / 2, PADDLE_THICKNESS, PADDLE_HEIGHT, 'white');
+    // Player 1 paddle
+    createCanvas(0, paddleY1, PADDLE_THICKNESS, PADDLE_HEIGHT, 'white');
 }
 
 function createCanvas(x, y, width, height, color) {
@@ -49,9 +58,22 @@ function createCanvas(x, y, width, height, color) {
     canvasContext.fillRect(x, y, width, height);
 }
 
+// Create ball
 function ballCanvas() {
     canvasContext.fillStyle = 'white';
     canvasContext.beginPath();
     canvasContext.arc(ballX, ballY, Math.PI * 2, 20, 10, true)
     canvasContext.fill();
+}
+
+// Calculate Mouse position on mouse move
+function calculateMousePosition(evt) {
+    let rect = canvas.getBoundingClientRect()
+    let root = document.documentElement;
+    let mouseX = evt.clientX - rect.left - root.clientLeft;
+    let mouseY = evt.clientY - rect.top - root.clientTop;
+    return {
+        x: mouseX,
+        y: mouseY
+    }
 }
